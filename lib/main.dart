13a1882e02be6284ed54b699dd06da2eaf6df641
Mohammmad-UltraCode <35679008+mohammad-dc@users.noreply.github.com>
2021-11-06 06:01:44 +0200

    regsiter screen design done 1st

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      initialRoute: '/login',
+      initialRoute: '/register',
       getPages: Routes.routes,
     );
   }
--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,10 +1,116 @@
+import 'package:fertilizer_pin/widgets/Form_Field.dart';
+import 'package:fertilizer_pin/widgets/button.dart';
+import 'package:fertilizer_pin/widgets/logo.dart';
+import 'package:fertilizer_pin/widgets/switch_to_signup.dart';
 import 'package:flutter/material.dart';
+import 'package:fertilizer_pin/widgets/fertilizer_text.dart';
 
 class RegisterScreen extends StatelessWidget {
   const RegisterScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 50,
+                  ),
+                  Center(child: const Logo()),
+                  SizedBox(
+                    height: 50,
+                  ),
+                  FertilizerText(
+                    text: 'حساب جديد',
+                    fontSize: 20,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    child: Container(
+                      child: Column(
+                        children: [
+                          FertilizerFormField(
+                            hintText: 'الاسم بالكامل',
+                            keyboardType: TextInputType.text,
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          FertilizerFormField(
+                            hintText: 'البريد الالكتروني',
+                            keyboardType: TextInputType.emailAddress,
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          FertilizerFormField(
+                            hintText: 'رقم الهوية',
+                            keyboardType: TextInputType.text,
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          FertilizerFormField(
+                            hintText: 'رقم الهاتف',
+                            keyboardType: TextInputType.phone,
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          FertilizerFormField(
+                            hintText: 'المدينة',
+                            keyboardType: TextInputType.text,
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          FertilizerFormField(
+                            hintText: 'العنوان بالتفصيل',
+                            keyboardType: TextInputType.text,
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          FertilizerFormField(
+                            hintText: 'كلمة المرور',
+                            keyboardType: TextInputType.text,
+                            obscureText: true,
+                          ),
+                          SizedBox(
+                            height: 30,
+                          ),
+                          FertilizerButton(text: 'تسجيل'),
+                          SizedBox(
+                            height: 40,
+                          ),
+                          FertilizerAuthLink(
+                              staticText: 'لديك حساب بالفعل؟',
+                              linkText: 'دخول'),
+                          SizedBox(
+                            height: 20,
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }

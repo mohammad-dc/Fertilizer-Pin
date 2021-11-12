bbb9c99798d88e1774f97950776a3e1bdac11fd3
Mohammmad-UltraCode <35679008+mohammad-dc@users.noreply.github.com>
2021-11-12 18:25:43 +0200

    auth controller done 1st

--- lib/controllers/auth/auth.dart
+++ lib/controllers/auth/auth.dart
@@ -1,5 +1,8 @@
 import 'dart:async';
 
+import 'package:fertilizer_pin/models/res/login/login.dart';
+import 'package:fertilizer_pin/models/res/login/response.dart';
+import 'package:fertilizer_pin/models/res/register/register.dart';
 import 'package:fertilizer_pin/models/res/verify/response.dart';
 import 'package:fertilizer_pin/models/res/verify/verify.dart';
 import 'package:fertilizer_pin/models/user/user.dart';
@@ -11,6 +14,13 @@ class AuthController extends GetxController with StateMixin<dynamic> {
   var verifySuccess =
       Verify(success: false, response: VerifyResponse(result: User())).obs();
 
+  var loginSuccess =
+      Login(success: false, response: LoginResponse(result: User())).obs();
+
+  var registerSuccess = Register(success: false, message: '').obs();
+
+  var authService = AuthServices();
+
   @override
   void onInit() {
     verify();
@@ -18,8 +28,6 @@ class AuthController extends GetxController with StateMixin<dynamic> {
   }
 
   void verify() async {
-    print('object');
-    var authService = AuthServices();
     var response = await authService.verifyAccount();
 
     if (response != null) {
@@ -29,6 +37,28 @@ class AuthController extends GetxController with StateMixin<dynamic> {
       } else if (response is Verify) {
         verifySuccess = response;
         print(verifySuccess);
+      }
+    }
+  }
+
+  void login(body) async {
+    var response = await authService.login(body);
+    if (response != null) {
+      if (response is Error) {
+        print(response.message);
+      } else if (response is Login) {
+        loginSuccess = response;
+      }
+    }
+  }
+
+  void register(body) async {
+    var response = await authService.register(body);
+    if (response != null) {
+      if (response is Error) {
+        print(response.message);
+      } else if (response is Register) {
+        registerSuccess = response;
       }
     }
   }
